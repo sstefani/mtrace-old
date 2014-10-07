a5b0fbd0cb988a0959bb4dff8e3e9bbd7244457a
Stefani Seibold <stefani@seibold.net>
2014-10-07 08:27:33 +0200

    add GPL V3 license, add source file headers

--- client/process.h
+++ client/process.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 Stefani Seibold <stefani@seibold.net>
+ *
+ * sponsored by Rohde & Schwarz GmbH & Co. KG
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef __PROCESS_H__
 #define __PROCESS_H__
 
